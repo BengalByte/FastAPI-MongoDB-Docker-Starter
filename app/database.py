60a35f6a2609bece300e6fe486b2c3d15f97aafa
Workshop User <workshop.user@amazon.com>
2024-03-10 22:47:49 +0600

    refactor: restructure

--- app/database.py
+++ app/database.py
@@ -1,13 +1,33 @@
 import os
+
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 
 load_dotenv()
 
+
+class DataBase:
+    pass
+    # def __init__(self, client):
+    #     self.client = client
+    #     self.db = client.college
+    #     self.student_collection = self.db.get_collection("students")
+    #     self.user_collection = self.db.get_collection("users")
+    #     self.account_details_collection = self.db.get_collection("accountDetails")
+    #     self.student_collection.create_index("roll_no")
+    #     self.user_collection.create_index("email")
+    #     self.account_details_collection.create_index("account_number")
+    #     self.account_details_collection.create_index("ifsc")
+    #     self.account_details_collection.create_index("branch")
+    #     self.account_details_collection.create_index("name")
+    #     self.account_details_collection.create_index("email")
+    #     self.account_details_collection.create_index("phone_number")
+
 USER = os.getenv("MONGOUSERNAME")
 PASSWORD = os.getenv("MONGOPASSWORD")
+MONGODB_URL = os.getenv("MONGODB_URL")
 
-MONGODB_URL = f"mongodb+srv://{USER}:{PASSWORD}@cluster0.fojn3vj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGODB_URL = f"mongodb+srv://{USER}:{PASSWORD}@cluster0.fojn3vj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" if not MONGODB_URL else MONGODB_URL
 
 client = AsyncIOMotorClient(MONGODB_URL)
 db = client.college
--- app/main.py
+++ app/main.py
@@ -1,11 +1,63 @@
-from fastapi import FastAPI
-from app.routes.students import students_router
-from app.routes.users import user_router
+from fastapi import FastAPI, status
+from fastapi.exceptions import HTTPException, RequestValidationError
+from fastapi.responses import JSONResponse
 
-app = FastAPI(
-    title="Student Course API",
-    summary="A sample application showing how to use FastAPI to add a ReST API to a MongoDB collection.",
-)
+from app.auth.routes.route_users import user_router
 
-app.include_router(students_router)
-app.include_router(user_router)
+
+def include_router(app, *routers):
+    for router in routers:
+        app.include_router(router)
+
+
+def add_middleware(app, middleware):
+    for md in middleware:
+        app.add_middleware(md)
+
+
+def add_exception_handler(app, exception_handler):
+    for ex, handler in exception_handler.items():
+        app.add_exception_handler(ex, handler)
+
+
+def http_error_handler(request, exc):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.detail}
+    )
+
+def validation_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"message": exc.errors()}
+    )
+
+def exception_handler():
+    return {
+        HTTPException: http_error_handler,
+        RequestValidationError: validation_exception_handler
+    }
+
+
+def configure_routes(app):
+    routers = [user_router]
+    include_router(app, *routers)
+
+
+def create_app():
+    app = FastAPI(
+        title="Student Course API",
+        summary="A sample application showing how to use FastAPI to add a ReST API to a MongoDB collection.",
+    )
+    configure_routes(app)
+    add_middleware(app, [])
+    add_exception_handler(app, exception_handler())
+
+    return app
+
+
+app = create_app()
+if __name__ == "__main__":
+    app = create_app()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
