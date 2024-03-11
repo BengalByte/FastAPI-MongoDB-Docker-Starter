5b88503f203245d1b304ac24837995b13e190903
Workshop User <workshop.user@amazon.com>
2024-03-11 11:51:31 +0600

    refactor: user models

--- app/auth/models/account_details.py
+++ app/auth/models/account_details.py
@@ -6,10 +6,12 @@ from pydantic.functional_validators import BeforeValidator
 
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
+
 class Gender(str, Enum):
     MALE = "male"
     FEMALE = "female"
     OTHER = "other"
+
 
 class AccountDetailsModel(BaseModel):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
@@ -19,14 +21,14 @@ class AccountDetailsModel(BaseModel):
     gender: Gender | None = Field(None)
     email: EmailStr | None = Field(None)
     contactNumber: str | None = Field(None)
-    companyNumber: str  | None = Field(None)
-    companyName: str  | None = Field(None)
-    vatNumber: str  | None = Field(None)
-    contactName: str  | None = Field(None)
-    billlingEmail: EmailStr  | None = Field(None)
-    maillingAddress: str  | None = Field(None)
-    createdAt: str  | None = Field(None)
-    updatedAt: str  | None = Field(None)
+    companyNumber: str | None = Field(None)
+    companyName: str | None = Field(None)
+    vatNumber: str | None = Field(None)
+    contactName: str | None = Field(None)
+    billlingEmail: EmailStr | None = Field(None)
+    maillingAddress: str | None = Field(None)
+    createdAt: str | None = Field(None)
+    updatedAt: str | None = Field(None)
     model_config = ConfigDict(
         populate_by_name=True,
         arbitrary_types_allowed=True,
@@ -43,23 +45,53 @@ class AccountDetailsModel(BaseModel):
             "billlingEmail": "johndoe@email.com",
             "maillingAddress": "123, Example Street, Example City, Example Country",
             "createdAt": "2021-07-01",
-            "updatedAt": "2021-07-01"
-        }
+            "updatedAt": "2021-07-01",
+        },
     )
 
 
 class AccountDetailCreateModel(BaseModel):
+    userID: Optional[PyObjectId] = Field(default=None)
     name: str | None = Field(None)
-    image: str | None
-
+    image: str | None = Field(None)
+    gender: Gender | None = Field(None)
+    email: EmailStr | None = Field(None)
+    contactNumber: str | None = Field(None)
+    companyNumber: str | None = Field(None)
+    companyName: str | None = Field(None)
+    vatNumber: str | None = Field(None)
+    contactName: str | None = Field(None)
+    billlingEmail: EmailStr | None = Field(None)
+    maillingAddress: str | None = Field(None)
+    createdAt: str | None = Field(None)
+    updatedAt: str | None = Field(None)
     model_config = ConfigDict(
         populate_by_name=True,
         arbitrary_types_allowed=True,
         json_schema_extra={
             "name": "John Doe",
-            "image": "https://www.example.com/image.jpg"
-        }
+            "image": "https://www.example.com/image.jpg",
+            "gender": "male",
+            "email": "johndoe@email.com",
+            "contactNumber": "1234567890",
+            "companyNumber": "1234567890",
+            "companyName": "John Doe Company",
+            "vatNumber": "1234567890",
+            "contactName": "John Doe",
+            "billlingEmail": "johndoe@email.com",
+            "maillingAddress": "123, Example Street, Example City, Example Country",
+            "createdAt": "2021-07-01",
+            "updatedAt": "2021-07-01",
+        },
     )
+
+
+class AccountDetailUpdateModel(AccountDetailCreateModel):
+    id: PyObjectId = Field(alias="_id")
+
+
+class AccountDetailResponseModel(AccountDetailCreateModel):
+    id: PyObjectId = Field(alias="_id")
 
 
 class AccountDetailsCollection(BaseModel):
